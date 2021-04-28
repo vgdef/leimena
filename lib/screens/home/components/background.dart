ef70a00ca7e084f340d31ca892d9d9e683730785
vgdef <valensyagdef@icloud.com>
2021-04-28 20:22:20 +0900

    kembali lagi

--- lib/screens/home/components/background.dart
+++ lib/screens/home/components/background.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:leimena/constants.dart';
 
 class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      color: Colors.white,
+      height: size.height * 0.9,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+              bottom: 36 + kDefaultPadding,
+            ),
+            height: size.height * 0.8 - 120,
+            decoration: BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+          ),
+          child,
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:leimena/components/already_have_an _account_acheck.dart';
+import 'package:leimena/components/rounded_input_field.dart';
+import 'package:leimena/components/rounded_button.dart';
+import 'package:leimena/screens/home/components/background.dart';
+import 'package:leimena/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                top: 290,
+              ),
+            ),
+            Text(
+              "Absensi Kerja",
+              style: GoogleFonts.lato(
+                color: kForthColor,
+                fontSize: 35,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
